530324517904636cfe70a39477d0f0592a5c2941
Dika Afty <dikaafty@gmail.com>
2025-10-01 23:34:35 +0700

    Use SF Pro Display Regular font classname in RootLayout body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`${SFProDisplayRegular.className} antialiased`}
       >
         {children}
       </body>
