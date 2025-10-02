ec3f5aa4e7b407700699b49d47df56fcc2aa498c
Dika Afty <dikaafty@gmail.com>
2025-10-02 23:43:42 +0700

    Import Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Header from "./components/Header/Header";
 import "./globals.css";
 
 const SFProDisplayRegular = localFont({
