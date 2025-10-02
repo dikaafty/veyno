5ecac255d0ea496872526bf7337ec655e9bf9b8f
Dika Afty <dikaafty@gmail.com>
2025-10-02 21:10:40 +0700

    Add navigation links constant

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    title: "Overview",
+    src: "/overview",
+  },
+  {
+    title: "Cashflow",
+    src: "/cashflow",
+  },
+  {
+    title: "Budget Planner",
+    src: "/budget-planner",
+  },
+  {
+    title: "Invoicing",
+    src: "/invoicing",
+  },
+  {
+    title: "Reports",
+    src: "/reports",
+  },
+];
