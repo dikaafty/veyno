5ff809b0bd969b0c573a5abf9d35ab7f18859c38
Dika Afty <dikaafty@gmail.com>
2025-10-03 13:44:23 +0700

    Add classnames to button jsx in AddTransaction component

--- src/app/components/Header/AddTransaction.tsx
+++ src/app/components/Header/AddTransaction.tsx
@@ -1,6 +1,9 @@
 const AddTransaction = () => {
   return (
-    <button>
+    <button
+      className="bg-secondary-card-background py-3 px-8 rounded-4xl text-sm cursor-pointer
+      shadow-[inset_0_0_2px_1px_rgb(255,255,255)] relative z-10 hidden lg:flex-center gap-1.5"
+    >
       
     </button>
   )
