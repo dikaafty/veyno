4b80ceba9e9382bbf97af9ecfc53a036ff97efcd
Dika Afty <dikaafty@gmail.com>
2025-10-04 01:36:32 +0700

    Add Props type for props of Hamburger component

--- src/app/components/Header/Hamburger.tsx
+++ src/app/components/Header/Hamburger.tsx
@@ -1,6 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 import { cn } from "@/app/lib/utils";
 
+type Props = {
+  isOpen: boolean,
+  toggle: Dispatch<SetStateAction<boolean>>,
+}
+
 const Hamburger = () => {
   return (
     <button>
