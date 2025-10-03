671ab74282a5d90e4a021ec6f87333e2590fca01
Dika Afty <dikaafty@gmail.com>
2025-10-03 13:47:29 +0700

    Add text content of button jsx in AddTransaction component

--- src/app/components/Header/AddTransaction.tsx
+++ src/app/components/Header/AddTransaction.tsx
@@ -12,6 +12,8 @@ const AddTransaction = () => {
           clipPath: "path('M 9 0 A 9 9 0 1 1 8.99 0 Z M 8 5 L 8 8 L 5 8 L 5 10 L 8 10 L 8 13 L 10 13 L 10 10 L 13 10 L 13 8 L 10 8 L 10 5 Z')",
         }}
       />
+
+      Add Transaction
     </button>
   )
 }
