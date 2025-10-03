1723bb294b641fc1595ed04f910a818bbcada5a0
Dika Afty <dikaafty@gmail.com>
2025-10-03 13:45:41 +0700

    Add div jsx (Button Gradient) with its classname in AddTransaction component

--- src/app/components/Header/AddTransaction.tsx
+++ src/app/components/Header/AddTransaction.tsx
@@ -4,7 +4,7 @@ const AddTransaction = () => {
       className="bg-secondary-card-background py-3 px-8 rounded-4xl text-sm cursor-pointer
       shadow-[inset_0_0_2px_1px_rgb(255,255,255)] relative z-10 hidden lg:flex-center gap-1.5"
     >
-      
+      <div className="button-gradient"  />
     </button>
   )
 }
