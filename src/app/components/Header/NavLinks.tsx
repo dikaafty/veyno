85e7582daf531f301b44a0df9a2fbabf10953d2d
Dika Afty <dikaafty@gmail.com>
2025-10-04 14:07:33 +0700

    Add Props type for props type of NavLinks component

--- src/app/components/Header/NavLinks.tsx
+++ src/app/components/Header/NavLinks.tsx
@@ -1,6 +1,12 @@
 import { navLinks } from "@/app/lib/constants";
 import { Dispatch, SetStateAction } from "react";
 
+type Props = {
+  isOpen: boolean,
+  activeLink: string,
+  setActiveLink: Dispatch<SetStateAction<string>>
+};
+
 const NavLinks = () => {
   return (
     <>
