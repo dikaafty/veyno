f4ac7ca5ad7953246305f4e3298907cb07e1a749
Dika Afty <dikaafty@gmail.com>
2025-10-04 14:01:42 +0700

    Update aria-controls value of button jsx to mobile-menu in Hamburger component

--- src/app/components/Header/Hamburger.tsx
+++ src/app/components/Header/Hamburger.tsx
@@ -12,7 +12,7 @@ const Hamburger = ({ isOpen, toggle }: Props) => {
       className="flex flex-col gap-1 sm:hidden cursor-pointer"
       onClick={() => toggle(prev => !prev)}
       aria-expanded={isOpen}
-      aria-controls="nav-menu"
+      aria-controls="mobile-menu"
     >
       <div className={cn("hamburger-stripe", isOpen && "rotate-45 translate-y-1.5")} />
       <div className={cn("hamburger-stripe", isOpen && "opacity-0")} />
