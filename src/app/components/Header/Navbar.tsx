eeb52ae815ab43510342d17a3289466a861a4929
Dika Afty <dikaafty@gmail.com>
2025-10-05 20:37:22 +0700

    Import gsap

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -4,6 +4,7 @@ import { navLinks } from "@/app/lib/constants";
 import { useState, useEffect } from "react";
 import { cn } from "@/app/lib/utils";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Link from "next/link";
 
 const Navbar = () => {
