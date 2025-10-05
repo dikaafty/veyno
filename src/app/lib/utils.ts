8c332c2d6aab9339db04d595baab663bd2f205f7
Dika Afty <dikaafty@gmail.com>
2025-10-05 19:48:21 +0700

    Add capitalize function to make the first letter of a string capitalized

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs));
 }
+
+export const capitalize = (str: string): string => {
+  return (str[0]?.toUpperCase() || "") + str.slice(1).toLowerCase();
+}
