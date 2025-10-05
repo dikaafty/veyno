0818d236754deb0cb784dc3dbcb6dab2f1740d98
Dika Afty <dikaafty@gmail.com>
2025-10-05 22:00:31 +0700

    Add div jsx (Left Items Container) with its classnames in Header component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -6,7 +6,9 @@ import ProfileHeader from "./ProfileHeader";
 const Header = () => {
   return (
     <header className="py-5 px-12 flex justify-center md:justify-between items-center">
-      
+      <div className="flex-center flex-col md:flex-row gap-6 md:gap-2">
+        
+      </div>
     </header>
   )
 }
