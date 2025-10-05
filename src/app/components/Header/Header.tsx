17f27230cfb3e9cc4c1c5048a188758c95957320
Dika Afty <dikaafty@gmail.com>
2025-10-05 22:01:27 +0700

    Render Image component for veyno logo with its attributes in Header component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,7 +7,13 @@ const Header = () => {
   return (
     <header className="py-5 px-12 flex justify-center md:justify-between items-center">
       <div className="flex-center flex-col md:flex-row gap-6 md:gap-2">
-        
+        <Image 
+          src="/images/veyno-logo.png"
+          width={42}
+          height={42}
+          alt="Veyno logo"
+          priority
+        />
       </div>
     </header>
   )
