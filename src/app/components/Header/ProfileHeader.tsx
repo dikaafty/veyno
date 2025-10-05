fd3eaee5b879bfb1acb7626ca56f7d6c183fe0f2
Dika Afty <dikaafty@gmail.com>
2025-10-05 21:53:37 +0700

    Add classnames to div jsx (Profile Header) in ProfileHeader component

--- src/app/components/Header/ProfileHeader.tsx
+++ src/app/components/Header/ProfileHeader.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 const ProfileHeader = () => {
   return (
-    <div>
+    <div 
+      className="grid grid-cols-[repeat(2,_max-content)] grid-rows-2 items-center gap-x-3 select-none 
+      cursor-pointer max-md:hidden"
+    >
       
     </div>
   )
