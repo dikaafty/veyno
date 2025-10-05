5d703230ead5bddb00486f7e305fc9ae395f52a8
Dika Afty <dikaafty@gmail.com>
2025-10-05 20:55:33 +0700

    Add opening GSAP animation to mobile menu if isMenuOpen state has true value in Navbar component

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -16,6 +16,11 @@ const Navbar = () => {
   useGSAP(() => {
     if(isMenuOpen) {
       gsap.set("#mobile-menu", { display: "flex" });
+
+      gsap.fromTo("#mobile-menu", 
+        { height: 0, opacity: 0, },
+        { height: "auto", opacity: 1, duration: 1, ease: "power1.inOut", }
+      );
     }
   }, [isMenuOpen]);
 
