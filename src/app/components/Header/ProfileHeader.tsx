c373dc67efde9fe8dad4859511df3503a0d7dcd8
Dika Afty <dikaafty@gmail.com>
2025-10-05 21:54:24 +0700

    Render Image component for photo profile with its attributes in ProfileHeader component

--- src/app/components/Header/ProfileHeader.tsx
+++ src/app/components/Header/ProfileHeader.tsx
@@ -6,7 +6,13 @@ const ProfileHeader = () => {
       className="grid grid-cols-[repeat(2,_max-content)] grid-rows-2 items-center gap-x-3 select-none 
       cursor-pointer max-md:hidden"
     >
-      
+      <Image 
+        src="/images/default-photo-profile.png"
+        alt="photo profile"
+        width={35}
+        height={35}
+        className="row-span-2 rounded-full outline-2 outline-white"
+      />
     </div>
   )
 }
