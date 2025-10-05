88d8f14d791656fe4eb748865bbf1d792bf9d120
Dika Afty <dikaafty@gmail.com>
2025-10-05 21:59:05 +0700

    Import ProfileHeader component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./Navbar";
 import AddTransaction from "./AddTransaction";
+import ProfileHeader from "./ProfileHeader";
 
 const Header = () => {
   return (
