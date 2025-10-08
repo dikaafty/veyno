22daf6debff58afbc6eec5f5ac13a726760f9481
Dika Afty <dikaafty@gmail.com>
2025-10-08 15:37:54 +0700

    Add typed useAppDispatch hook for type-safe dispatching

--- src/app/store/redux/hooks.ts
+++ src/app/store/redux/hooks.ts
@@ -2,3 +2,4 @@ import { type TypedUseSelectorHook, useSelector, useDispatch } from "react-redux
 import type { RootState, AppDispatch } from "./store";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
