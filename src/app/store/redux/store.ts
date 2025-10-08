7f07887c8c7adf69f4cc5fc86bfe16e28618bb9f
Dika Afty <dikaafty@gmail.com>
2025-10-08 15:33:03 +0700

    Configure Redux store with user and transactions reducers

--- src/app/store/redux/store.ts
+++ src/app/store/redux/store.ts
@@ -1,3 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/app/features/user/userSlice";
 import transactionsReducer from "@/app/features/transactions/transactionsSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    transactions: transactionsReducer,
+  }
+});
