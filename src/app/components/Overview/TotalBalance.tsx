ed4cd2544e5ad59ba073595dff92e2dc30f80eff
Dika Afty <dikaafty@gmail.com>
2025-10-09 22:58:45 +0700

    Add attributes and its value to article jsx in TotalBalance component

--- src/app/components/Overview/TotalBalance.tsx
+++ src/app/components/Overview/TotalBalance.tsx
@@ -2,7 +2,10 @@ import { SFProDisplayLight, SFProDisplayThin } from "@/app/lib/fonts";
 
 const TotalBalance = () => {
   return (
-    <article>
+    <article 
+      aria-labelledby="total-balance" 
+      className="bg-primary-card-background card grid-balance"
+    >
       
     </article>
   )
