7751f4a84fa4f3fcb8f95a0af4db7e5434d49a19
Dika Afty <dikaafty@gmail.com>
2025-10-14 15:31:37 +0700

    Import ChartOptions and ChartConfiguration types

--- src/app/components/Overview/CashflowSummaryChart.tsx
+++ src/app/components/Overview/CashflowSummaryChart.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Chart as ChartJS, registerables } from "chart.js";
+import { Chart as ChartJS, registerables, type ChartOptions, type ChartConfiguration } from "chart.js";
 import { Bar } from "react-chartjs-2";
 
 const CashflowSummaryChart = () => {
