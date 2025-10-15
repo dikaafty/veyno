429d9f5d4bca2c9f3d1c9834b55c308663499055
Dika Afty <dikaafty@gmail.com>
2025-10-15 16:08:47 +0700

    Add cashflowMock constant (Temporary)

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -20,3 +20,26 @@ export const navLinks = [
     src: "/reports",
   },
 ];
+
+export const cashflowMock = [
+  {
+    revenue: 1240,
+    expenses: 528,
+  },
+  {
+    revenue: 3128,
+    expenses: 153,
+  },
+  {
+    revenue: 1500,
+    expenses: 1400,
+  },
+  {
+    revenue: 978,
+    expenses: 2923,
+  },
+  {
+    revenue: 600,
+    expenses: 838,
+  },
+];
