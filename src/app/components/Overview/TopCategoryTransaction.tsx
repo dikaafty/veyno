3f01c48739616ebe0b8fb795be08c2d9e78dbd70
Dika Afty <dikaafty@gmail.com>
2025-10-15 23:26:36 +0700

    Import TransactionCategories type

--- src/app/components/Overview/TopCategoryTransaction.tsx
+++ src/app/components/Overview/TopCategoryTransaction.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAppSelector } from "@/app/store/redux/hooks";
+import { TransactionCategories } from "@/app/lib/types";
 
 const TopCategoryTransaction = () => {
   return (
