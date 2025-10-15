7f3ce1bd0d675a708d62111315f698880902a247
Dika Afty <dikaafty@gmail.com>
2025-10-15 16:39:29 +0700

    Import CashflowMock type

--- src/app/components/Overview/CashflowSummary.tsx
+++ src/app/components/Overview/CashflowSummary.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { cn } from "@/app/lib/utils";
 import { cashflowMock } from "@/app/lib/constants";
+import { CashflowMock } from "@/app/lib/types";
 
 const CashflowSummary = () => {
   return (
