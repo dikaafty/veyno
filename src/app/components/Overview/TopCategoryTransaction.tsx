df385383bf04635ae5cb41181c55c448560018bd
Dika Afty <dikaafty@gmail.com>
2025-10-15 23:30:35 +0700

    Add div jsx (Card Gradient) with its classnames in TopCategoryTransaction

--- src/app/components/Overview/TopCategoryTransaction.tsx
+++ src/app/components/Overview/TopCategoryTransaction.tsx
@@ -20,7 +20,7 @@ const TopCategoryTransaction = () => {
       aria-labelledby="transaction-heading" 
       className="bg-primary-card-background card relative z-10 col-span-2 gap-5"
     >
-      
+      <div className="card-gradient rounded-2xl -z-10" />
     </section>
   )
 }
