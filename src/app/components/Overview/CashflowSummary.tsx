47b2bd8fe0639cc0139c35a3d35cc487fbcdc0cd
Dika Afty <dikaafty@gmail.com>
2025-10-15 16:39:50 +0700

    Import CashflowSummaryChart component

--- src/app/components/Overview/CashflowSummary.tsx
+++ src/app/components/Overview/CashflowSummary.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { cn } from "@/app/lib/utils";
 import { cashflowMock } from "@/app/lib/constants";
 import { CashflowMock } from "@/app/lib/types";
+import CashflowSummaryChart from "./CashflowSummaryChart";
 
 const CashflowSummary = () => {
   return (
