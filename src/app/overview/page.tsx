b2ad69ff59b7cd697a6a6da9ac3426357bdb19c5
Dika Afty <dikaafty@gmail.com>
2025-10-16 23:48:01 +0700

    Import Revenue component

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -2,6 +2,7 @@ import Greeting from "../components/Overview/Greeting";
 import CashflowSummary from "../components/Overview/CashflowSummary";
 import TotalBalance from "../components/Overview/TotalBalance";
 import Expenses from "../components/Overview/Expenses";
+import Revenue from "../components/Overview/Revenue";
 
 export default function Page() {
   return (
