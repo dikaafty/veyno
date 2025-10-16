145bc5b6306b5fdc484575656ad9b65f24f732c8
Dika Afty <dikaafty@gmail.com>
2025-10-16 23:47:45 +0700

    Import Expenses component

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,6 +1,7 @@
 import Greeting from "../components/Overview/Greeting";
 import CashflowSummary from "../components/Overview/CashflowSummary";
 import TotalBalance from "../components/Overview/TotalBalance";
+import Expenses from "../components/Overview/Expenses";
 
 export default function Page() {
   return (
