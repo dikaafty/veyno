15ec51b8bdcec34c8ac96690b62038438e741df9
Dika Afty <dikaafty@gmail.com>
2025-10-16 23:47:28 +0700

    Import TotalBalance component

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,5 +1,6 @@
 import Greeting from "../components/Overview/Greeting";
 import CashflowSummary from "../components/Overview/CashflowSummary";
+import TotalBalance from "../components/Overview/TotalBalance";
 
 export default function Page() {
   return (
