febb1522e8eef3475c2f8bb957687b5d57918844
Dika Afty <dikaafty@gmail.com>
2025-10-16 23:48:22 +0700

    Import TopCategoryTransaction component

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -3,6 +3,7 @@ import CashflowSummary from "../components/Overview/CashflowSummary";
 import TotalBalance from "../components/Overview/TotalBalance";
 import Expenses from "../components/Overview/Expenses";
 import Revenue from "../components/Overview/Revenue";
+import TopCategoryTransaction from "../components/Overview/TopCategoryTransaction";
 
 export default function Page() {
   return (
