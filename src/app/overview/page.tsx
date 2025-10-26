fd12819c4ed308c2c50507644d537f1b6e9d7a99
Dika Afty <dikaafty@gmail.com>
2025-10-26 23:22:40 +0700

    Add classnames to main jsx in Page

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -7,7 +7,11 @@ import TopCategoryTransaction from "../components/Overview/TopCategoryTransactio
 
 export default function Page() {
   return (
-    <main>
+    <main 
+      className="px-6 py-9 grid grid-cols-4 grid-rows-[0.6fr_2fr_0.5fr_0.5fr_0.5fr_1fr] 
+      xs:grid-rows-[0.6fr_3fr_0.8fr_0.8fr_1.5fr] md:grid-rows-[1.5fr_4fr_1.5fr_3fr] 
+      lg:grid-cols-[3fr_1fr_1fr] lg:grid-rows-[1.5fr_1.5fr_3fr] gap-2"
+    >
       <Greeting />
       <CashflowSummary />
       <TotalBalance />
