839488a483320951f4b41f313e14d12c108e9599
Dika Afty <dikaafty@gmail.com>
2025-10-27 13:48:31 +0700

    Move components folder outside the app folder (To src folder)

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,9 +1,9 @@
-import Greeting from "../components/Overview/Greeting";
-import CashflowSummary from "../components/Overview/CashflowSummary";
-import TotalBalance from "../components/Overview/TotalBalance";
-import Expenses from "../components/Overview/Expenses";
-import Revenue from "../components/Overview/Revenue";
-import TopCategoryTransaction from "../components/Overview/TopCategoryTransaction";
+import Greeting from "@/components/Overview/Greeting";
+import CashflowSummary from "@/components/Overview/CashflowSummary";
+import TotalBalance from "@/components/Overview/TotalBalance";
+import Expenses from "@/components/Overview/Expenses";
+import Revenue from "@/components/Overview/Revenue";
+import TopCategoryTransaction from "@/components/Overview/TopCategoryTransaction";
 
 export default function Page() {
   return (
