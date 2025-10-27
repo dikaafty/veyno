686709c56bb7496480488da9331ddd9e63fd38f6
Dika Afty <dikaafty@gmail.com>
2025-10-27 13:50:58 +0700

    Move features folder outside app folder (To src folder)

--- src/app/store/redux/store.ts
+++ src/app/store/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "@/app/features/user/userSlice";
-import transactionsReducer from "@/app/features/transactions/transactionsSlice";
+import userReducer from "@/features/user/userSlice";
+import transactionsReducer from "@/features/transactions/transactionsSlice";
 
 export const store = configureStore({
   reducer: {
