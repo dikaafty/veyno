468802f7bfd2da954025a8bd5a4fbe6630b5a2a3
Dika Afty <dikaafty@gmail.com>
2025-11-06 20:47:08 +0700

    Remove rounded-3xl classname from div jsx (Card Gradient) in Revenue

--- src/components/Overview/Revenue.tsx
+++ src/components/Overview/Revenue.tsx
@@ -6,7 +6,7 @@ const Revenue = () => {
       aria-labelledby="revenue"
       className="bg-tertiary-card-background card relative grid-revenue max-xs:col-span-4 max-md:col-span-2 max-lg:col-span-1"
     >
-      <div className="card-gradient rounded-3xl" />
+      <div className="card-gradient" />
 
       <div className="flex flex-col gap-1.5">
         <h3 id="expenses" className="text-sm">
