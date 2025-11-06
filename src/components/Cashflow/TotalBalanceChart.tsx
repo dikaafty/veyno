d8127bc018553917d306153fbe63a930b89a72d3
Dika Afty <dikaafty@gmail.com>
2025-11-06 22:01:35 +0700

    Import fullCasflowMock constant

--- src/components/Cashflow/TotalBalanceChart.tsx
+++ src/components/Cashflow/TotalBalanceChart.tsx
@@ -1,4 +1,5 @@
 import { Chart as ChartJS, registerables, type ChartOptions, type ChartData } from "chart.js";
+import { fullCasflowMock } from "@/lib/constants";
 
 const TotalBalanceChart = () => {
   return (
