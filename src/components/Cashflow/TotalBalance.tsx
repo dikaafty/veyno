ba37bb674a0fe24c008cf899cf6eda04439760cb
Dika Afty <dikaafty@gmail.com>
2025-11-06 21:37:44 +0700

    Import TrendingUp component

--- src/components/Cashflow/TotalBalance.tsx
+++ src/components/Cashflow/TotalBalance.tsx
@@ -1,11 +1,12 @@
 import { SFProDisplayLight, SFProDisplayRegular } from "@/lib/fonts";
+import { TrendingUp } from "lucide-react";
 
 const TotalBalance = () => {
   return (
     <section>
       
     </section>
-  )
-}
+  );
+};
 
 export default TotalBalance;
