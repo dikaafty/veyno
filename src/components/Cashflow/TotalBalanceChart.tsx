e00277fcaaa34afa350e8d385ff50189dd51e15b
Dika Afty <dikaafty@gmail.com>
2025-11-07 18:26:12 +0700

    Add logic to extract last week's expenses data from fullCashflowMock

--- src/components/Cashflow/TotalBalanceChart.tsx
+++ src/components/Cashflow/TotalBalanceChart.tsx
@@ -8,6 +8,7 @@ import { fullCasflowMock } from "@/lib/constants";
 
 const TotalBalanceChart = () => {
   const lastWeekRevenue = fullCasflowMock.slice(2).map(item => item.revenue);
+  const lastWeekExpenses = fullCasflowMock.slice(2).map(item => item.revenue);
 
   return (
     <>
