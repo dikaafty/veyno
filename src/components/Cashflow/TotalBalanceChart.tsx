ea01b6b8919804dc7252d30b23172b637c4fa9fc
Dika Afty <dikaafty@gmail.com>
2025-11-08 14:34:54 +0700

    Add getWeeklyMovingAverage function and its logic to generate weekly moving average

--- src/components/Cashflow/TotalBalanceChart.tsx
+++ src/components/Cashflow/TotalBalanceChart.tsx
@@ -16,6 +16,19 @@ const TotalBalanceChart = () => {
   const lastWeekRevenue = lastWeekCashflow.map((item) => item.revenue);
   const lastWeekExpenses = lastWeekCashflow.map((item) => item.expenses);
 
+  const getWeeklyMovingAverage = (data: Data[]) => {
+    const netBalances = data.map((item) => item.revenue - item.expenses);
+    const windowSize = 3;
+
+    return netBalances
+      .map((_, i, arr) => {
+        const start = Math.max(0, i - (windowSize - 1));
+        const slice = arr.slice(start, i + 1);
+        return Math.round(slice.reduce((a, b) => a + b, 0) / slice.length);
+      })
+      .slice(2);
+  };
+
   return (
     <>
 
