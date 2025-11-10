9de9c6d6d0b88363dfdc78e1bc08f970ecb05463
Dika Afty <dikaafty@gmail.com>
2025-11-10 16:34:55 +0700

    Add div jsx (Card Gradient) with its classnames in TotalBalance

--- src/components/Cashflow/TotalBalance.tsx
+++ src/components/Cashflow/TotalBalance.tsx
@@ -8,6 +8,7 @@ const TotalBalance = () => {
       aria-labelledby="total-balance"
       className="bg-primary-card-background card relative z-10 flex flex-col gap-5"
     >
+      <div className="card-gradient -z-10" />
     </section>
   );
 };
