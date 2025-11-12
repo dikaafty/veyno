2b937a2b51401dc8601c801f8cfaa592f4c80d5f
Dika Afty <dikaafty@gmail.com>
2025-11-12 19:54:36 +0700

    Add classnames to section jsx in PerformanceMetrics

--- src/components/Cashflow/PerformanceMetrics.tsx
+++ src/components/Cashflow/PerformanceMetrics.tsx
@@ -2,6 +2,7 @@ const PerformanceMetrics = () => {
   return (
     <section
       aria-labelledby="performance-metrics"
+      className="bg-primary-card-background card relative z-10 flex flex-col"
     >
       
     </section>
