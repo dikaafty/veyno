29620192ba2b2cd942b7b7f9389b75301e69df50
Dika Afty <dikaafty@gmail.com>
2025-11-24 14:07:38 +0700

    Add getChartColors with its type in DonutStatsCardChartProps

--- src/components/Cashflow/DonutStatsCardChart.tsx
+++ src/components/Cashflow/DonutStatsCardChart.tsx
@@ -13,6 +13,7 @@ type DonutStatsCardChartProps = {
   title: string;
   total: string;
   items: { title: string; value: number }[];
+  getChartColors: () => string[];
 };
 
 type CenterTextLine = {
