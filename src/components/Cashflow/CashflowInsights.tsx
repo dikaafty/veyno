be7ac81865ce37f9085d11b01f0c3314ca6640b3
Dika Afty <dikaafty@gmail.com>
2025-11-26 22:02:30 +0700

    Add classnames to section jsx in CashflowInsights

--- src/components/Cashflow/CashflowInsights.tsx
+++ src/components/Cashflow/CashflowInsights.tsx
@@ -4,6 +4,7 @@ const CashflowInsights = () => {
   return (
     <section
       aria-labelledby="cashflow-insights"
+      className="bg-primary-card-background card relative z-10 flex flex-col gap-3"
     >
       
     </section>
