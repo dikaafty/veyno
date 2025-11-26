094f755e0ebb7b3d93302fdd5c513a29a2d0c8cd
Dika Afty <dikaafty@gmail.com>
2025-11-26 22:12:41 +0700

    Add insightsMock array with its items (Temporary)

--- src/components/Cashflow/CashflowInsights.tsx
+++ src/components/Cashflow/CashflowInsights.tsx
@@ -1,6 +1,29 @@
 import { ArrowDown, TrendingUp, ShieldCheck } from "lucide-react";
 import CashflowInsightsCard from "./CashflowInsightsCard";
 
+const insightsMock = [
+  {
+    title: "Expenses Dropped",
+    icon: ArrowDown,
+    description:
+      "Expenses dropped by 12% vs last month. Keep up the great work!",
+    score: null,
+  },
+  {
+    title: "Largest Revenue Source",
+    icon: TrendingUp,
+    description:
+      "Client Projects was your largest revenue source this month, contributing to 60% of total revenue.",
+    score: null,
+  },
+  {
+    title: "Cashflow Stability Score",
+    icon: ShieldCheck,
+    description: "Cashflow stability score:",
+    score: 8.7,
+  },
+];
+
 const CashflowInsights = () => {
   return (
     <section
