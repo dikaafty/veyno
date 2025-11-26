f24179ed2a1766d1840c3af10c4baa9fe0e940e9
Dika Afty <dikaafty@gmail.com>
2025-11-26 22:16:36 +0700

    Add div jsx (Cashflow Insights Card Container) with its classnames in CashflowInsights

--- src/components/Cashflow/CashflowInsights.tsx
+++ src/components/Cashflow/CashflowInsights.tsx
@@ -28,13 +28,17 @@ const CashflowInsights = () => {
   return (
     <section
       aria-labelledby="cashflow-insights"
-      className="bg-primary-card-background card relative z-10 flex flex-col gap-3"
+      className="bg-primary-card-background card relative z-10 flex flex-col gap-3 col-span-2"
     >
       <div className="card-gradient -z-10" />
 
       <h1 id="cashflow-insights" className="text-lg">
         Insights
       </h1>
+
+      <div className="grid grid-cols-3 gap-3">
+        
+      </div>
     </section>
   );
 };
